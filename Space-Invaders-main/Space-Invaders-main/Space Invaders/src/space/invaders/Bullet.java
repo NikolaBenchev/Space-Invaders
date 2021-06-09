9747628edf672100051f0c8edf39f03f95894a13
NikolaBenchev <71229286+NikolaBenchev@users.noreply.github.com>
2021-06-09 16:27:00 +0300

    Delete Space-Invaders-main/Space-Invaders-main/Space Invaders directory

--- Space-Invaders-main/Space-Invaders-main/Space Invaders/src/space/invaders/Bullet.java
+++
@@ -1,9 +0,0 @@
-package space.invaders;
-
-public class Bullet extends Unit{
-
-    Bullet(Position position, int hp, int speed, String name){
-        super(position, hp, speed, name);
-        setSpeed(speed);
-    }
-}
