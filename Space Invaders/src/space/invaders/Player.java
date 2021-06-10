515091981d5da473d7e825ce4890a20df73d92cd
totoo7 <58186680+totoo7@users.noreply.github.com>
2021-06-10 13:06:08 +0300

    Add files via upload

--- Space Invaders/src/space/invaders/Player.java
+++ Space Invaders/src/space/invaders/Player.java
@@ -6,15 +6,33 @@ public class Player extends Unit{
     private int score;
     private float currentAttackTime;   
     private float attackSpeed = 0.5f;
+    private boolean movingLeft = false;
+    private boolean movingRight = false;
+
+    public boolean isMovingLeft() {
+        return movingLeft;
+    }
+
+    public void setMovingLeft(boolean movingLeft) {
+        this.movingLeft = movingLeft;
+    }
+
+    public boolean isMovingRight() {
+        return movingRight;
+    }
+
+    public void setMovingRight(boolean movingRight) {
+        this.movingRight = movingRight;
+    }
     
-    Player(Position position, int Hp, int speed, String name){
-        super(position, Hp, speed, name);
+    Player(Position position, int Hp, int speed, String name, int size){
+        super(position, Hp, speed, name, size);
         this.score = 0;
         this.currentAttackTime = 0.5f;
     }   
     
     public Bullet shoot(){
-        Bullet bullet = new Bullet(this.getPosition(), 1, -8, "playerBullet");
+        Bullet bullet = new Bullet(this.getPosition(), 1, -8, "playerBullet", Main.SIZE);
         return bullet;
     }
     
